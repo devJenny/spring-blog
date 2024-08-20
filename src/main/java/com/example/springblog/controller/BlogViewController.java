a890940e2cdea1c484edf750e11fd909fce666da
devjenny <devje@naver.com>
2024-08-20 23:48:19 +0900

    Feat: Created board list

---
+++ src/main/java/com/example/springblog/controller/BlogViewController.java
@@ -0,0 +1,29 @@
+package com.example.springblog.controller;
+
+import com.example.springblog.dto.ArticleListViewResponse;
+import com.example.springblog.service.BlogService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List ;
+
+@RequiredArgsConstructor
+@Controller
+public class BlogViewController {
+
+    private final BlogService blogService;
+
+    @GetMapping("/articles")
+    public String getArticles(Model model) {
+        List<ArticleListViewResponse> articles = blogService.findAll()
+                .stream()
+                .map(ArticleListViewResponse::new)
+                .toList();
+
+        model.addAttribute("articles", articles);
+
+        return "articleList";
+    }
+}
